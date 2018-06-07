4eb3483e028a30d87f228cfbaf40ea419f1d6fc0
Xingyu Chen <xc77@duke.edu>
2018-06-07 08:24:56 -0400

    Add Dataset_1

--- local_search.py
+++ local_search.py
@@ -3,7 +3,7 @@ import random
 
 def local_search(data_list, k):
     num = len(data_list)
-    random_begin = [random.randint(1, num) for x in range(k)]
+    random_begin = [random.randint(0, num-1) for x in range(k)]
     cur_acc = calculate_accuracy(data_list, random_begin, k)
     cur_state = random_begin
     while True:
@@ -13,7 +13,7 @@ def local_search(data_list, k):
                 temp[x] = i
                 local_move_acc = calculate_accuracy(data_list, temp, k)
                 if local_move_acc > 1.05 * cur_acc:
-                    local_move_acc = 1.05* cur_acc
+                    local_move_acc = 1.05 * cur_acc
                     cur_state = temp
         break
     return local_move_acc
